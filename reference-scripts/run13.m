061fb7d37993aa5b74a5e1a0e4699ad26630075a
Lightmann <lightmanns11@gmail.com>
2026-06-06 19:24:09 -0700

    Remove unrelated ref scripts

--- reference-scripts/run13.m
+++
@@ -1,5 +0,0 @@
-% Pd La RIXS
-go_Pd_La
-mv m1pitch -6.379 %non-res
-andorSet numKins 1; andorSet setshutter 0; pause(2);
-rixs SNSdotPdPPh3_20mM_PdLa_RIXS_01 3160 2 3172 0.5 3174 0.25 3175 .15 3179 0.25 3181 0.5 3186 1 3202 2 3210 5 3300 3
--- reference-scripts/run9.m
+++
@@ -1,6 +0,0 @@
-% RIXS
-go_S_Ka
-mv m1pitch -6.479 %Sulfur RIXS + resonant XES
-andorSet numKins 1; andorSet setshutter 0; pause(2);
-rixs SNSdotNiPPh3_10mM_SKa_RIXS_02 2463 1 2468 0.5 2470 .1 2476 .5 2483 1 2498 5 2538 7
-andorSet setShutter 0 numKins 1
